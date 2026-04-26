25c99170960e02da3244327291c8c650c5b50419
kundanjan <kundan7058@gmail.com>
2026-04-26 23:26:09 +0530

    Jay Ganesh

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+
+using namespace std;
+
+
+
+class ArrayStack{
+    private:
+        int top;
+        
+
+    public:
+    ArrayStack(){
+        top = -1;
+    }
+
+    int push(int iValue)
+    {
+
+    }
+
+    int pop()
+    {
+
+    }
+
+    int peek()
+    {
+
+    }
+
+};
+
+int main()
+{
+    ArrayStack st;
+    return 0;
+}
